ba4429e85a7548136bc749b105c7d11a9cb57896
Usman Suleiman <usmansbk@gmail.com>
2021-09-28 21:16:22 +0100

    Style service

--- components/Home/index.js
+++ components/Home/index.js
@@ -1,17 +1,31 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 import Avatar from "../common/Avatar";
 import IconButton from "../common/IconButton";
 import styles from "./styles";
 
-const Header = () => {
-  const name = "One Achmad";
-  const picture = "https://i.pravatar.cc/300";
+const Service = ({ services = [] }) => (
+  <View style={styles.serviceContainer}>
+    <Text style={styles.serviceTitle}>Service</Text>
+    <View style={styles.services}>
+      {services.map(({ id, name, icon, color }) => (
+        <TouchableOpacity key={id} style={styles.serviceItem}>
+          <View style={[styles.serviceIcon, { backgroundColor: color }]}>
+            <Ionicons name={icon} color="white" size={40} />
+          </View>
+          <Text style={styles.serviceName}>{name}</Text>
+        </TouchableOpacity>
+      ))}
+    </View>
+  </View>
+);
 
+const Header = ({ avatar, name }) => {
   return (
     <View style={styles.header}>
       <View style={styles.headerLeft}>
-        <Avatar uri={picture} />
+        <Avatar uri={avatar} />
         <View style={styles.text}>
           <Text style={styles.salutation}>Hello</Text>
           <Text style={styles.name}>{name}</Text>
@@ -23,9 +37,38 @@ const Header = () => {
 };
 
 export default function Home() {
+  const name = "One Achmad";
+  const picture = "https://i.pravatar.cc/300";
+  const services = [
+    {
+      id: "wallet",
+      name: "Wallet",
+      icon: "wallet",
+      color: "#ef5160",
+    },
+    {
+      id: "transfer",
+      name: "Transfer",
+      icon: "cash",
+      color: "#85d0f9",
+    },
+    {
+      id: "pay",
+      name: "Pay",
+      icon: "card",
+      color: "#fbb244",
+    },
+    {
+      id: "topup",
+      name: "Topup",
+      icon: "phone-portrait",
+      color: "#50b350",
+    },
+  ];
   return (
     <View style={styles.container}>
-      <Header />
+      <Header name={name} avatar={picture} />
+      <Service services={services} />
     </View>
   );
 }
--- components/Home/styles.js
+++ components/Home/styles.js
@@ -25,4 +25,31 @@ export default StyleSheet.create({
     marginLeft: 8,
     justifyContent: "space-between",
   },
+  serviceContainer: {
+    marginHorizontal: 16,
+  },
+  serviceTitle: {
+    fontWeight: "bold",
+    marginBottom: 16,
+  },
+  services: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  serviceItem: {
+    width: 80,
+    height: 80,
+  },
+  serviceIcon: {
+    padding: 16,
+    borderRadius: 16,
+    marginBottom: 16,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  serviceName: {
+    textAlign: "center",
+    fontWeight: "bold",
+    color: "#888",
+  },
 });
