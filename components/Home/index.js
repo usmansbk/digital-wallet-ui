25418531a3e5760664b9cbcd3f469c5e1cfa351b
Usman Suleiman <usmansbk@gmail.com>
2021-09-28 23:48:17 +0100

    Display location

--- components/Home/index.js
+++ components/Home/index.js
@@ -1,6 +1,7 @@
 import { useScrollToTop } from "@react-navigation/native";
-import React, { useRef } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { View, Text, StyleSheet, ScrollView } from "react-native";
+import * as Location from "expo-location";
 import Avatar from "../common/Avatar";
 import IconButton from "../common/IconButton";
 import CardList from "./Cards";
@@ -22,101 +23,118 @@ const Header = ({ avatar, name, salutation }) => {
   );
 };
 
-export default function Home() {
-  const salutation = "Hello";
-  const name = "One Achmad";
-  const picture = "https://i.pravatar.cc/300";
-  const services = [
-    {
-      id: "wallet",
-      name: "Wallet",
-      icon: "wallet",
-      color: "#ef5160",
-    },
-    {
-      id: "transfer",
-      name: "Transfer",
-      icon: "cash",
-      color: "#85d0f9",
-    },
-    {
-      id: "pay",
-      name: "Pay",
-      icon: "card",
-      color: "#fbb244",
-    },
-    {
-      id: "topup",
-      name: "Topup",
-      icon: "phone-portrait",
-      color: "#50b350",
-    },
-  ];
-  const transactions = [
-    {
-      id: "dribbble",
-      title: "Dribbble",
-      subtitle: "Payment Received",
-      amount: "$245",
-      color: "#fd5c94",
-      icon: "dribbble",
-    },
-    {
-      id: "google-wallet",
-      title: "Google Wallet",
-      subtitle: "Payment via wallet can be done",
-      amount: "$180",
-      color: "#64af25",
-      icon: "google-wallet",
-    },
-    {
-      id: "figma",
-      title: "Uplabs",
-      subtitle: "Payment Received",
-      amount: "$137",
-      color: "#93a7e7",
-      img: require("../../assets/uplabs.jpg"),
-    },
-  ];
-  const cards = [
-    {
-      id: "card-1",
-      type: "master-card",
-      currency: "USD",
-      amount: 248.75,
-      owner: "One Achmad",
-      exp: "10/28",
-      number: "****  ****  ****  1234",
-      color: "#fbb245",
-    },
-    {
-      id: "card-2",
-      type: "master-card",
-      currency: "USD",
-      amount: 248.75,
-      owner: "One Achmad",
-      exp: "10/28",
-      number: "****  ****  ****  1234",
-      color: "#4965fb",
-    },
-    {
-      id: "card-3",
-      type: "visa",
-      currency: "USD",
-      amount: 248.75,
-      owner: "One Achmad",
-      exp: "10/28",
-      number: "****  ****  ****  1234",
-      color: "#40b7fb",
-    },
-  ];
+const name = "One Achmad";
+const picture = "https://i.pravatar.cc/300";
+const services = [
+  {
+    id: "wallet",
+    name: "Wallet",
+    icon: "wallet",
+    color: "#ef5160",
+  },
+  {
+    id: "transfer",
+    name: "Transfer",
+    icon: "cash",
+    color: "#85d0f9",
+  },
+  {
+    id: "pay",
+    name: "Pay",
+    icon: "card",
+    color: "#fbb244",
+  },
+  {
+    id: "topup",
+    name: "Topup",
+    icon: "phone-portrait",
+    color: "#50b350",
+  },
+];
+const transactions = [
+  {
+    id: "dribbble",
+    title: "Dribbble",
+    subtitle: "Payment Received",
+    amount: "$245",
+    color: "#fd5c94",
+    icon: "dribbble",
+  },
+  {
+    id: "google-wallet",
+    title: "Google Wallet",
+    subtitle: "Payment via wallet can be done",
+    amount: "$180",
+    color: "#64af25",
+    icon: "google-wallet",
+  },
+  {
+    id: "figma",
+    title: "Uplabs",
+    subtitle: "Payment Received",
+    amount: "$137",
+    color: "#93a7e7",
+    img: require("../../assets/uplabs.jpg"),
+  },
+];
+const cards = [
+  {
+    id: "card-1",
+    type: "master-card",
+    currency: "USD",
+    amount: 248.75,
+    owner: "One Achmad",
+    exp: "10/28",
+    number: "****  ****  ****  1234",
+    color: "#fbb245",
+  },
+  {
+    id: "card-2",
+    type: "master-card",
+    currency: "USD",
+    amount: 248.75,
+    owner: "One Achmad",
+    exp: "10/28",
+    number: "****  ****  ****  1234",
+    color: "#4965fb",
+  },
+  {
+    id: "card-3",
+    type: "visa",
+    currency: "USD",
+    amount: 248.75,
+    owner: "One Achmad",
+    exp: "10/28",
+    number: "****  ****  ****  1234",
+    color: "#40b7fb",
+  },
+];
 
+export default function Home() {
   const ref = useRef(null);
   useScrollToTop(ref);
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        alert("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({
+        accuracy: Location.Accuracy.Lowest,
+      });
+      const matches = await Location.reverseGeocodeAsync(location.coords);
+      const address = matches[0];
+      setLocation(`${address.city}, ${address.country}`);
+    })();
+  }, []);
 
   return (
     <View style={styles.container}>
-      <Header salutation={salutation} name={name} avatar={picture} />
+      <Header salutation={location} name={name} avatar={picture} />
       <ScrollView ref={ref}>
         <CardList cards={cards} />
         <Service services={services} />
